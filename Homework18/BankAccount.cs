ba09b7b8d68eb6907f7631ad2cd5b4c5162e381a
mishantique <michvitko@mail.ru>
2024-02-22 15:30:35 +0300

    Добавьте файлы проекта.

---
+++ Homework18/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework18
+{
+    internal class BankAccount <T>
+    {
+        public T Number { get; set; }
+        public int Balance { get; set; }
+        public string Name { get; set; }
+
+        public void Input()
+        {
+            Console.WriteLine("Введите номер счёта");
+            Number = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Введите количество средств");
+            Balance = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ФИО");
+            Name = Console.ReadLine();
+        }
+        public string GetInfo()
+        {
+            return $"Номер счёта: {Number}, Баланс: {Balance}, Имя: {Name}";
+        }
+    }
+}
---
+++ Homework18/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount<int> account1 = new BankAccount<int>();
+            account1.Input();
+            Console.WriteLine(account1.GetInfo());
+
+            BankAccount<string> account2 = new BankAccount<string>();
+            account2.Input();
+            Console.WriteLine(account2.GetInfo());
+
+            Console.ReadKey();
+        }
+    }
+}
